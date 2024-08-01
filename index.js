2256012dc9241bed533b582a438454ef98450604
TheSanchari <sanchari678@gmail.com>
2024-08-01 23:33:14 +0530

    implemented push

--- index.js
+++ index.js
@@ -11,8 +11,25 @@ class stack {
         this.top = newnode
         this.length = 1
     }
+
+    push(value) {
+        const newnode = new Node(value)
+        if(this.length === 0) {
+            this.top = newnode
+            this.top.next = null
+            this.length = 1
+        } else {
+        let temp = this.top
+        this.top = newnode
+        this.top.next = temp
+        this.length++
+        }
+        return this
+    }
     
 }
 
 const newStack = new stack(11)
-console.log(newStack)
+// console.log(newStack)
+console.log(newStack.push(12))
+console.log(newStack.push(14))
